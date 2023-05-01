26b5bfa2bc96edf1f00de6d472a79ca73322304a
Nathaniel Pellegrino <62487654+natep-tech@users.noreply.github.com>
2023-04-30 23:27:32 -0400

    Add SMS capabilities

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/:number', (req, res) => {
   res.redirect(`tel:${phone}`);
 });
 
+app.get('/sms/:number', (req, res) => {
+  const { number } = req.params;
+  const phone = number.replace(/[^0-9]/g, '');
+  res.redirect(`sms:${phone}`);
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`App is running`);
 });
