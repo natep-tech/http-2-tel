b21e6586a8ec41482ae13896005be9a33d65d456
Nathaniel Pellegrino <62487654+natep-tech@users.noreply.github.com>
2023-04-30 23:14:58 -0400

    Set code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+
+app.get('/:number', (req, res) => {
+  const { number } = req.params;
+  const phone = number.replace(/[^0-9]/g, '');
+  res.redirect(`tel:${phone}`);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is running`);
+});
